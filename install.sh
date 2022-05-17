5eb40118f31915506ed756e8b4ae3071d84378db
Kentaro Fujita <50708397+kentaro-fujita@users.noreply.github.com>
2022-05-18 00:02:27 +0900

    update: modules

--- install.sh
+++ install.sh
@@ -9,13 +9,7 @@ ln -sf "$HOME/dotfiles/.zplug" "$HOME/.zplug"
 ln -sf "$HOME/dotfiles/.fzf" "$HOME/.fzf"
 ln -sf "$HOME/dotfiles/.vim" "$HOME/.vim"
 ln -sf "$HOME/dotfiles/.zshrc" "$HOME/.zshrc"
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  ln -sf "$HOME/dotfiles/.tmux.conf_mac" "$HOME/.tmux.conf"
-else
-  ln -sf "$HOME/dotfiles/.tmux" "$HOME/.tmux"
-  ln -sf "$HOME/dotfiles/.tmux.conf" "$HOME/.tmux.conf"
-fi
+ln -sf "$HOME/dotfiles/.tmux.conf" "$HOME/.tmux.conf"
 
 if [ ! -d "$HOME/.config"]; then
   mkdir "$HOME/.config"
