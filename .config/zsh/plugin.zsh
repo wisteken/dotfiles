b5b8eaa17a360b5a290bb9ae83e83c65ea204e88
Kentaro Fujita <50708397+kentaro-fujita@users.noreply.github.com>
2022-05-22 05:13:36 +0900

    Update: git completion

--- .config/zsh/plugin.zsh
+++ .config/zsh/plugin.zsh
@@ -2,6 +2,11 @@ zinit light zsh-users/zsh-history-substring-search
 zinit light zsh-users/zsh-syntax-highlighting
 zinit light zsh-users/zsh-autosuggestions
 zinit light zsh-users/zsh-completions
+
+zinit wait silent lucid atclone"zstyle ':completion:*:*:git:*' script git-completion.bash" atpull"%atclone" for \
+  "https://github.com/git/git/blob/master/contrib/completion/git-completion.bash"
+zinit wait lucid as"completion" atload"zicompinit; zicdreplay" mv"git-completion.zsh -> _git" for \
+  "https://github.com/git/git/blob/master/contrib/completion/git-completion.zsh"
+
 zinit snippet OMZL::git.zsh
 zinit snippet PZTM::helper
-
